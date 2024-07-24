bb0adeed5c292334672fd33e38710c94a5dc651f
Himanshu Kesharwani <>
2024-07-24 12:04:42 +0530

    [ADD]: Project setup initial commit.

---
+++ Swiftui-Framework-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftui-Framework-App
+//
+//  Created by Himanshu Kesharwani on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftui-Framework-App/Swiftui_Framework_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Framework_AppApp.swift
+//  Swiftui-Framework-App
+//
+//  Created by Himanshu Kesharwani on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Framework_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
